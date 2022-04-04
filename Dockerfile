13651b78d3972b445648ffb6dc5711730e92b3c9
honeybadger26 <honeybadger26@gmail.com>
2022-04-04 23:56:13 +1000

    - Setup hot reloading - Fixed bugs in comprehension questions - Changed instruction design

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ WORKDIR /usr/src/app
 COPY requirements*.txt .
 RUN pip install -r requirements.txt
 
-CMD sh -c 'otree prodserver 0.0.0.0:8000'
+# CMD sh -c 'otree prodserver 0.0.0.0:8000'
+CMD sh -c 'otree devserver 0.0.0.0:8000'
--- thesis_instructions/pages.py
+++ thesis_instructions/pages.py
@@ -1,7 +1,8 @@
-from otree.api import Currency as c, currency_range
-from ._builtin import Page, WaitPage
-from .models import Constants
+from ._builtin import Page
 
+UNDERSTOOD_QUESTIONS = ['understood1', 'understood11', 'understood12',
+        'understood13', 'understood2', 'understood21', 'understood22', 'understood23',
+        'understood24']
 
 class InformedConsent(Page):
     form_model = 'player'
@@ -15,33 +16,30 @@ class InformationBrochure(Page):
 
 class Instructions(Page):
     form_model = 'player'
-    form_fields = ['comprehension1',
-                   'comprehension2',
-                   'comprehension3',
-                   'comprehension4',
-                   'comprehension5',
-                   'comprehension6',
-                   'understood1',
-                   'understood11',
-                   'understood12',
-                   'understood13',
-                   'understood2',
-                   'understood21',
-                   'understood22',
-                   'understood23',
-                   'understood24']
+    form_fields = UNDERSTOOD_QUESTIONS + ['comprehension1', 'comprehension2', 'comprehension3',
+        'comprehension4', 'comprehension5', 'comprehension6']
 
-    def understood_error_message(self, values):
-        if values['self.player.understood1'] is None or values['self.player.understood11'] is None or values['self.player.understood12'] is None or \
-                values['self.player.understood13'] is None or values['self.player.understood2'] is None or values['self.player.understood21'] is None or \
-                values['self.player.understood22'] is None or values['self.player.understood23'] is None or values['self.player.understood24'] is None:
-            return 'Tick all the boxes in the subsections to show you read and understood the instructions.'
+    def error_message(self, values):
+        for field_name in UNDERSTOOD_QUESTIONS:
+            if values[field_name] is None:
+                return 'Tick all the boxes in the subsections to show you read and understood the instructions.'
 
-    def comprehension_error_message(self, values):
-        if values['self.player.comprehension1'] != 2 or values['self.player.comprehension2'] != 2 or values['self.player.comprehension3'] != 1 \
-                or values['self.player.comprehension4'] != 1 or values['self.player.comprehension5'] != 3 or \
-                values['self.player.comprehension6'] != 3:
-            return 'At least one answer is wrong. Please try again.'
+        solutions = dict(
+            comprehension1=2,
+            comprehension2=2,
+            comprehension3=1,
+            comprehension4=1,
+            comprehension5=3,
+            comprehension6=3
+        )
+            
+        error_messages = dict()
+
+        for field_name in solutions:
+            if values[field_name] != solutions[field_name]:
+                error_messages[field_name] = 'This answer is wrong'
+        
+        return error_messages
 
 
 page_sequence = [
