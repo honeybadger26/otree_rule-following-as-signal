56c21e3a5810deb7479acad07fe24336305105d1
honeybadger26 <honeybadger26@gmail.com>
2022-04-13 21:12:01 +1000

    debriefing cleanup

---
+++ thesis_debriefing/__init__.py
@@ -0,0 +1,29 @@
+from otree.api import *
+
+# Models #
+
+class C(BaseConstants):
+    NAME_IN_URL = 'thesis_debriefing'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    show_payoff = models.BooleanField()
+
+
+# Pages #
+
+class Debriefing(Page):
+    pass
+
+
+page_sequence = [Debriefing]
