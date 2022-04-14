b742164d7156956bbf29b58324cabacacfe9b0f4
honeybadger26 <honeybadger26@gmail.com>
2022-04-14 19:32:20 +1000

    Cleaned up deployment

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM python:3.9
 
 WORKDIR /usr/src/app
 
-COPY requirements*.txt .
+COPY requirements.txt ./
 RUN pip install -r requirements.txt
 
-# CMD sh -c 'otree prodserver 0.0.0.0:8000'
+EXPOSE 8000
 CMD sh -c 'echo oTree app is running && otree devserver 0.0.0.0:8000'
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+case "$@" in
+    "")
+        docker build -t otree-rfas-dev .
+        docker run -it --rm -p 8000:8000 -e OTREE_PRODUCTION=0 -v $(pwd):/usr/src/app otree-rfas-dev
+        ;;
+    -p)
+        git clean -fdx
+        docker build -f Dockerfile.prod -t otree-rfas-prod .
+        docker run -it --rm -p 8000:8000 -e OTREE_PRODUCTION=1 otree-rfas-prod
+        ;;
+    -t*)
+        shift
+        docker build -t otree-rfas-test .
+        docker run -it --rm -v $(pwd):/usr/src/app otree-rfas-test otree test "$@"
+        ;;
+    *)
+        echo 'Error: Incorrect usage'
+        ;;
+esac
